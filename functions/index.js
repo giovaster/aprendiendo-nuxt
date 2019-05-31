22e17787b2172d727bbb649845f15bda8004164c
giovanny sterlin <giovannysterlingm@gmail.com>
2019-05-30 20:07:11 -0500

    aprendiento

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+const functions = require('firebase-functions');
+const slug = require('slug-generator')
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Holi perras!");
+});
+
+exports.onCreateProduct = functions.firestore.document("productos/{producto_id}").onCreate((snap, context) =>{
+    let newData = snap.data()
+    newData.slug = slug(newData.nombre)
+
+    return snap.ref.set(newData)
+})
+
+//cambia => onUpdate
--- services/firebase.js
+++ services/firebase.js
@@ -1,17 +1,26 @@
 import firebase from 'firebase'
+import 'firebase/storage'
+import 'firebase/auth'
+
 var config = {
-    apiKey: "AIzaSyA9IBh-ozZxQIZgTk56_uUh03ioziEwZ0c",
+    apiKey: "AIzaSyC5rxkzk_eVwI3dQvFaC89hcpVBn4zCEnA",
     authDomain: "nuxtbd.firebaseapp.com",
     databaseURL: "https://nuxtbd.firebaseio.com",
     projectId: "nuxtbd",
     storageBucket: "nuxtbd.appspot.com",
-    messagingSenderId: "964859792751"
+    messagingSenderId: "1021053836954"
   };
+
   if(!firebase.apps.length){
     firebase.initializeApp(config);
   }
   const db = firebase.firestore()
+  const storage = firebase.storage().ref()
+  const auth = firebase.auth()
+
   export default firebase
-  export{
-    db
+  export {
+    db,
+    storage,
+    auth
   }
