290fda7bfed731a907bf0342b72ce5224a82d6d0
giovanny sterlin <giovannysterlingm@gmail.com>
2019-05-30 20:33:57 -0500

    aprendiendo con ..

--- services/firebase.js
+++ services/firebase.js
@@ -4,10 +4,10 @@ import 'firebase/auth'
 
 var config = {
   apiKey: "AIzaSyC5rxkzk_eVwI3dQvFaC89hcpVBn4zCEnA",
-  authDomain: "nxut-50586.firebaseapp.com",
-  databaseURL: "https://nxut-50586.firebaseio.com",
-  projectId: "nxut-50586",
-  storageBucket: "nxut-50586.appspot.com",
+  authDomain: "nuxtbd.firebaseapp.com",
+  databaseURL: "https://nuxtbd.firebaseio.com",
+  projectId: "nuxtbd",
+  storageBucket: "nuxtbd.appspot.com",
   messagingSenderId: "1021053836954"
   };
 
